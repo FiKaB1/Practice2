c2c96998ce91826b2dda667864485d490b97fafd
Ali <a_kapan@kbtu.kz>
2026-01-31 14:55:34 +0500

    First

---
+++ Boolens.py
@@ -0,0 +1,37 @@
+print(10>0)
+
+print(5<9)
+
+print(2==2)
+
+a=5
+b=20
+if a>b :
+    print("a more than b")
+elif a==b :
+    print("equal")
+
+else :
+    print("b more than a")
+
+print(bool(1))
+print(bool("Hello"))
+
+print(False)
+print(bool(None))
+print(bool(0))
+print(bool({}))
+print(bool(()))
+print(bool([]))
+print(bool(""))
+print(bool(''))
+
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+x="Ali"
+print(isinstance(x,str))
+print(isinstance(x,int))
