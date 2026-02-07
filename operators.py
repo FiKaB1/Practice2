b93e451fdc120c862b565cc4237fa337b5ec099e
Ali <a_kapan@kbtu.kz>
2026-02-07 11:19:36 +0500

    Operators

---
+++ operators.py
@@ -0,0 +1,87 @@
+#Arithmetic Operators
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#Assignment Operators
+x=5
+x+=3
+print(x)
+x=5
+x-=3
+print(x)
+x=5
+x/=3
+print(x)
+x=5
+x*=3
+print(x)
+x=5
+x//=3
+print(x)
+x=5
+x**=3
+print(x)
+x=5
+x%=3
+print(x)
+x=5
+x&=3
+print(x)
+x=5
+x|=3
+print(x)
+x=5
+x^=3
+print(x)
+x=5
+x>>=3
+print(x)
+x=5
+x<<=3
+print(x)
+print(x:=3)
+#Comparison Operators
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+
+#Logical Operators
+print(1>2 and 2>1)
+print(1>2 or 2>1)
+print(not 1>2)
+
+#Identity Operators
+x = [1, 2, 3]
+y = [1, 2, 3]
+
+print(x == y)
+print(x is y)
+
+#Membership Operators
+x=[1,2,3,4]
+y=2
+z=5
+print(y in x)
+print(z in x)
+print(z not in x)
+
+#Bitwise Operators
+a=1
+b=2
+print(a&b)
+print(a^b)
+print(a|b)
+print(a>>2)
+print(a<<3)
+print(~a)
+
